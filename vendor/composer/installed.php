c3b4f245e6b6b15f0ed2a42aa65e5a9bd25a3ae0
Marz-dev-tech <85668522+Marz-dev-tech@users.noreply.github.com>
2024-08-12 13:11:24 +0300

    Mpesa libraries
    Addition of the Mpesa API

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'levizwannah/mpesa-sdk-php' => array(
+            'pretty_version' => 'v1.1.2.3',
+            'version' => '1.1.2.3',
+            'reference' => 'a0b4c3606c789e32886ce21808b251787f1c7b34',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../levizwannah/mpesa-sdk-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
